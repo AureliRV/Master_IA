d680aec32dcc5d0fabc73f77f183165223fce17b
aureli <aurelireberte@hotmail.com>
2024-04-07 22:52:29 +0200

    inicio EDA

---
+++ EDA/main.py
@@ -0,0 +1,16 @@
+#data manipulation
+import pandas as pd
+import numpy as np
+#data visualitzation
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+import seaborn as sns
+
+#plot styles
+plt.style.use("ggplot")
+#rcParams['figure.figuresize'] = (12, 6)
+
+#Import data set from sklearn
+from sklearn.datasets import load_wine
+print("Helo master!")
+
