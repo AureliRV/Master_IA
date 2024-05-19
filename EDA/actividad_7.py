833ec54cf64d5d4a21dce936416f420bdf9e5db3
aureli <aurelireberte@hotmail.com>
2024-05-19 19:38:48 +0200

    Fin actividad 7

--- EDA/actividad_7.py
+++ EDA/actividad_7.py
@@ -3,26 +3,22 @@
 import geopandas as gpd
 import pandas as pd
 import matplotlib.pyplot as plt
-# Cargar el conjunto de datos del mundo
-import geodatasets as ged
-from geodatasets import get_path
-
-
-#help(ged)
+#ayuda de Python
+#help()
 
 # Cargar el conjunto de datos del mundo
 
 world = 'C:/Users/Aureli/anaconda3/envs/MasterGeo/Lib/site-packages/geopandas/datasets/naturalearth_lowres/naturalearth_lowres.shp'
 gworld = gpd.read_file(world)
-#print(gworld.head())
-#print(gworld)
-gworld.plot()
+print(gworld.head())
+print(gworld)
+#gworld.plot()
 
-'''
+
 # Supongamos que tienes un DataFrame llamado 'data' que contiene información sobre la población más rica por país.
 
-data = pd.read_csv ("C:/Users/Aureli/Dropbox/Mi PC (DESKTOP-707RTP7)/Documents/MasterIA-Ioe-UDIMA/Python stuff/total_wealth.csv")
-# Asegúrate de tener una columna con el nombre 'population_wealthy'
+data = pd.read_csv ("C:/Users/Aureli/Github_IA/Actividad5/EDA/total_wealth.csv")
+
 print("--------------Describe---------------------")
 print(data.describe())
 print("--------------INFO-------------------------")
@@ -47,22 +43,27 @@ data_l["Mediana"] = data_l.mean(axis=1, numeric_only=True)
 print(data_l)
 print("--------------CRECIMIENTO 1998 a 2018-------------------------")
 data_l["Growth"] = data_l["2018"] - data_l["1998"]
-print(data_l)'''
+print(data_l)
+
+# Asegúrate de tener una columna con el nombre 'population_wealthy', yo he creado un dataset con el crecimiento económinco de los paises entre 1998 y 2018, sacado
+# de la web del banco muncidal y he creado la columna que me interesa: en este caso 'Mediana' 
 
 
-'''# Visualizar el mapa del mundo
-# Unir los datos del mapa con tu conjunto de datos (usando merge)
-world = world.merge(data, how='left', left_on='name', right_on='country')
+# Unir los datos del mapa con tu conjunto de datos (usando merge) NO lo hago, creo la columna de la mediana/por poblacion directamente.
+#nworld = gworld.merge(data_l, how='left', left_on='name', right_on='country')
 # Normalizar los datos de población rica para que el valor más bajo sea cero
-max_population = world['population_wealthy'].max()
-world['normalized_population'] = world['population_wealthy'] / max_population
+max_population = gworld['pop_est'].max()
+gworld['normalized_population'] = data_l['Mediana'] / max_population
+
+print("------------Población Máxima--------------")
+print(max_population)
+print("------------Nuevo Dataset con la mediana normaliada Población/mediana de riqueza--------------")
+print(gworld)
+
 # Crear el mapa con colores basados en la población rica
-world.plot(column='normalized_population', cmap='YlGnBu', legend=True)
+gworld.plot(column='normalized_population', cmap='YlGnBu', legend=True)
 # Mostrar el mapa
 plt.show()
 
-En este ejemplo, se utiliza GeoPandas para cargar el conjunto de datos del mundo y luego unirlo con el conjunto de
-datos que contiene información sobre la población más rica por país. Después, se normalizan los datos para que el valor
-más bajo sea cero y se representa el mapa del mundo con colores basados en la población más rica, utilizando un esquema de 
-color 'YlGnBu'. '''
+
 
