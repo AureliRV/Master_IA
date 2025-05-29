cd7d05142c27e3c5915b01d32189d97eef08d663
aureli <aurelireberte@hotmail.com>
2025-05-29 19:01:17 +0200

    front end v1 i formulari

--- AppNudisTFM/app.py
+++ AppNudisTFM/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hola món!"
+@app.route('/templates')
+def inici():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
